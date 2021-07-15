c296b94d99ca35e83e7683bb99be213eeb0ef30d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-07-15 12:54:17 +0530

    add changes in server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
 
+const connectDB = require("./server/database/connection");
+
 const app = express();
 
 dotenv.config({ path: "config.env" });
@@ -12,20 +14,23 @@ const PORT = process.env.PORT || 8080;
 // log requests
 app.use(morgan("tiny"));
 
+// mongodb connection
+connectDB();
+
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
 
 // set view engine
 app.set("view engine", "ejs");
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
 app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// load routers
+app.use("/", require("./server/routes/router"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
